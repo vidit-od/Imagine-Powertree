4604f4c66eac78b6c8c62b6b5257e593a662c45d
vidit-od <vidit894@gmail.com>
2023-02-16 15:46:17 +0530

    reserve appointment backend

--- code/full stack/Imagine_Powertree/app/models.py
+++ code/full stack/Imagine_Powertree/app/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 # Create your models here.
+class reserve(models.Model):
+    i_am_choice={
+        ('Commercial Customer',"Commercial Customer"),
+        ("Residentail Customer","Residentail Customer"),
+        ("Reseller","Reseller")
+    }
+    query_id=models.AutoField
+    i_am=models.CharField(max_length=30,blank=False,choices=i_am_choice)
+    name=models.CharField(max_length=100,blank=False,null=False)
+    email=models.EmailField(max_length=100,null=False,blank=False,unique=False)
+    contact=PhoneNumberField(null=False,blank=False,unique=False)
+    address_ln1=models.CharField(max_length=100,blank=False,null=False)
+    address_ln2=models.CharField(max_length=100,blank=True,null=True)
+    state=models.CharField(max_length=50,blank=False,null=False)
+    comment=models.CharField(max_length=500,blank=True,null=True,default=None)
+
+    def __str__(self):
+        return '%s || %s || %s' %(self.name,self.contact, self.pk)
+    
--- code/full stack/Imagine_Powertree/app/views.py
+++ code/full stack/Imagine_Powertree/app/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import auth,User
 from django.contrib import messages
 from datetime import datetime
 from django.urls import reverse
+from .models import reserve as reserve_model
 import os
 from django.conf import settings
 import random
@@ -13,6 +14,32 @@ def index(request):
     return render(request, 'index.html')
 
 def reserve(request):
+    if request.method=="POST":
+        i_am = request.POST["i_am"]
+        name = request.POST["full-name"]
+        email = request.POST["email"]
+        contact = request.POST["number"]
+        address_1 = request.POST["address_1"]
+        address_2 = request.POST["address_2"]
+        state = request.POST["state"]
+        comment = request.POST["comment"]
+
+        if( '@' not in email or '.com' not in email ):
+            messages.info(request, 'please enter proper email')
+            return redirect('reserve')
+
+        
+        if (not(contact[1:].isnumeric())):
+            messages.info(request, contact)
+            return redirect('reserve')
+
+        elif('+' not in contact):
+            contact="+"+contact
+        
+        data = reserve_model.objects.create(i_am=i_am, name=name, email=email, contact= contact, address_ln1=address_1, address_ln2=address_2, state=state, comment=comment)
+        data.save()
+        return redirect('reserve')
+
     return render(request, 'reserve.html')
 
 def why_powertree(request):
