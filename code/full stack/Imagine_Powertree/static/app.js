9e0cd051d3fbda3f46b02b746c31059b23b3b20c
vidit-od <vidit894@gmail.com>
2023-02-28 17:31:00 +0530

    home page responsive

--- code/full stack/Imagine_Powertree/static/app.js
+++ code/full stack/Imagine_Powertree/static/app.js
@@ -1,12 +1,12 @@
 var height = document.documentElement.scrollHeight
-console.log(height);
 
 const navbar =document.querySelector(".navbar");
 const home_title = document.querySelectorAll(".text_animation");
 const home_image = document.querySelector(".home_image");
 
-window/addEventListener('DOMContentLoaded',function(){
-    
+window.addEventListener('DOMContentLoaded',function(){
+    content_resize();
+    pageht();
     navbar.style.transform="translateY(0%)";    
     for(let i = 0; i< home_title.length; i++){
         home_title[i].style.transform ="translateY(0%)";
@@ -14,7 +14,10 @@ window/addEventListener('DOMContentLoaded',function(){
     home_image.style.transition="all 0.5s ease"
     home_image.style.opacity="1";
 })
-
+window.addEventListener('resize',function(){
+    content_resize();
+    pageht();
+})
 const navbar_content= document.querySelector(".content");
 const navbar_right =document.querySelector(".navright");
 const why_phowertree = document.querySelector(".why_powertree");
@@ -22,7 +25,10 @@ const backshade =document.querySelector(".backshade");
 const p3_image = document.querySelector(".p3_image");
 const p3_content = document.querySelector(".p3_content");
 const p4_lower =document.querySelector(".lower");
+
+
 window.addEventListener('scroll', function(){
+    if(window.scrollX >=2000){
     const percent= window.scrollY/height * 100;
     console.log(percent)
     // powertree 360 rotation animation
@@ -74,6 +80,7 @@ window.addEventListener('scroll', function(){
         document.querySelector(".banner").style.width="50%";
         document.querySelector(".arrow").style.width="0%";
     }
+    }
 })
 
 
@@ -82,20 +89,18 @@ let transform = 1;
 let images = document.querySelector(".images");
 let all_slider_images= document.querySelectorAll(".p3_images");
 
-let navbar_ht =document.querySelector(".navbar").scrollHeight;
-let page1 = document.querySelector(".home").scrollHeight;
-let page2 = document.querySelector(".page2").scrollHeight;
-let page3 = document.querySelector(".page3").scrollHeight;
-let page4 = document.querySelector(".page4").scrollHeight;
-let page5 = document.querySelector(".page5").scrollHeight;
-let banner = document.querySelector(".banner").scrollHeight;
-document.getElementById("footer").style.top=(navbar_ht+page1+page2+page3+page4+page5+banner+"px");
+
 
 images.innerHTML=images.innerHTML + "<div class='p3_images'>" +all_slider_images[0].innerHTML + "</div>";
 function next(){
     current = current+1;
-    transform = -580*current;
-    
+    if(window.outerWidth <= 1000){
+        transform = -window.outerWidth*current;
+        console.log("hi");
+    }
+    else{
+        transform = -580*current;
+    }
     let max = document.querySelectorAll(".p3_images").length;
     images.style.transition ="transform 2s ease";
     images.style.transform = "translateX("+ transform +"px)";
@@ -110,6 +115,40 @@ function next(){
     
 }
 
+
+function pageht(){
+    let navbar_ht =document.querySelector(".navbar").scrollHeight;
+    document.querySelector(".home").style.top = navbar_ht + "px";
+    let page1 = document.querySelector(".home").scrollHeight;
+    document.querySelector(".page2").style.top =navbar_ht + page1 +"px";
+    let page2 = document.querySelector(".page2").scrollHeight;
+    document.querySelector(".page3").style.top = navbar_ht + page1 + page2 + "px"; 
+    let page3 = document.querySelector(".page3").scrollHeight;
+    document.querySelector(".page4").style.top = navbar_ht + page1 + page2 + page3 + "px";
+    let page4 = document.querySelector(".page4").scrollHeight;
+
+    document.querySelector(".banner").style.top = navbar_ht + page1 + page2 + page3 + page4 + "px";
+    let banner = document.querySelector(".banner").scrollHeight;
+    
+    document.querySelector(".page5").style.top = navbar_ht + page1 + page2 + page3 + page4 +banner+ "px";
+    let page5 = document.querySelector(".page5").scrollHeight;
+    document.getElementById("footer").style.top=(navbar_ht + page1+ page2 + page3 + page4 + page5 + banner+"px");
+}
+function content_resize(){
+    if(window.outerWidth <= 1000){
+        console.log("yo");
+        document.querySelector(".p3_image").style.width = window.outerWidth + "px";
+        let all_images = document.querySelectorAll(".p3_images");
+        for(i =0; i< all_images.length; i++){
+            all_images[i].style.width = window.outerWidth + "px";
+        }
+
+        document.querySelector(".p3_content").style.width = window.outerWidth + "px";
+
+        //  console.log(document.querySelector(".footer_img").he, document.querySelector(".footer .upper").style.height )
+        document.querySelector(".footer_img").style.height = document.querySelector(".footer .upper").clientHeight + "px";
+    }
+}
 setInterval(function() {
     next()
   }, 5000);
