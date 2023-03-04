8e3041bb01f531893e99ad97b280caba8cddf25e
vidit-od <vidit894@gmail.com>
2023-03-04 16:41:33 +0530

    projects responsive

--- code/full stack/Imagine_Powertree/static/project.js
+++ code/full stack/Imagine_Powertree/static/project.js
@@ -10,19 +10,35 @@ texts.innerHTML = texts.innerHTML + "<div class='text'>"+ txts[0].innerHTML + "<
 imgs = document.querySelectorAll(".image");
 txts = document.querySelectorAll(".text");
 
-const navbar_height = document.querySelector(".navbar").scrollHeight;
-const page1 =document.querySelector(".page1").scrollHeight;
-const page2 =document.querySelector(".page2").scrollHeight;
-const page3 =document.querySelector(".page3").scrollHeight;
-document.getElementById("page4").style.top = navbar_height + page1 +page2 + page3 +"px";
-const page4 = document.querySelector(".page4").scrollHeight;
-document.getElementById("footer").style.top = navbar_height + page1 +page2 + page3 + page4 +"px";
-
 var current = 0;
 var transform =1;
+
+if(window.outerWidth <1000){
+    resp();
+}
+heightset()
+
+function resp(){
+    const image_frame = document.querySelector(".image_frame");
+    image_frame.style.width = window.outerWidth + "px";
+    const all_images = document.querySelectorAll(".image");
+    const all_texts = document.querySelectorAll(".text");
+    document.querySelector(".desc_frame").style.transform = "translateX(-"+ (window.outerWidth/2 + 10)+"px) translateY("+(0.6*window.outerHeight) +"px)";
+
+    for(let i = 0; i< imgs.length; i++){
+        all_images[i].style.width = window.outerWidth + "px";
+        all_texts[i].style.width = window.outerWidth + "px";
+        
+    }
+}
 function next(){
     current = current+1;
-    transform = -500*current;
+    if( window.outerWidth > 1000){
+        transform = -500*current;
+    }
+    else{
+        transform = -(window.outerWidth)*current;
+    }
     images.style.transition = "transform 2s ease";
     images.style.transform = "translateX(" + transform + "px)";
     texts.style.transition = "transform 2s ease";
@@ -32,15 +48,31 @@ function next(){
             images.style.transition = "none";
             texts.style.transition = "none";
             console.log("in");
-            current = 0;    
-            transform = -500*current;
+            current = 0;
+            if( window.outerWidth > 1000){
+                transform = -500*current;
+            }
+            else{
+                transform = -(window.outerWidth)*current;
+            }
             images.style.transform = "translateX(" + transform + "px)";
             texts.style.transform= "translateX(" + transform + "px)";
         }
     })
     
 }
-
-setInterval(function() {
-    next()
-  }, 5000);
+function heightset(){
+    const navbar_height = document.querySelector(".navbar").scrollHeight;
+    document.querySelector(".page1").style.top = navbar_height + "px";
+    const page1 =document.querySelector(".page1").scrollHeight;
+    document.querySelector(".page2").style.top = navbar_height +page1 +"px";
+    const page2 =document.querySelector(".page2").scrollHeight;
+    document.querySelector(".page3").style.top = navbar_height +page1+page2+ "px";
+    const page3 =document.querySelector(".page3").scrollHeight;
+    document.querySelector(".page4").style.top = navbar_height +page1+page2+page3+"px";
+    const page4 = document.querySelector(".page4").scrollHeight;
+    document.getElementById("footer").style.top = navbar_height + page1 +page2 + page3 + page4 +"px";
+}
+ setInterval(function() {
+         next()
+   }, 5000);
