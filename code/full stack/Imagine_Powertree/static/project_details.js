8257c3eb110ab8dc1c8d73fff7f54219c54e3776
vidit-od <vidit894@gmail.com>
2023-03-04 16:59:47 +0530

    project_details responsive

--- code/full stack/Imagine_Powertree/static/project_details.js
+++ code/full stack/Imagine_Powertree/static/project_details.js
@@ -3,6 +3,11 @@ const details_content = document.querySelector(".details");
 let details = details_content.innerHTML;
 details = details.replaceAll("&lt;","<");
 details = details.replaceAll("&gt;",">");
-
 details_content.innerHTML = details;
-console.log(details);
+
+
+let navht = document.querySelector(".navbar").scrollHeight;
+let page1 = document.querySelector(".main_content").scrollHeight;
+console.log(navht);
+console.log(page1);
+document.querySelector(".footer").style.top = navht + page1 + 20 +"px";
