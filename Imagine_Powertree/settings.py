d26cb576c4b1e4b257be79c3504f0eebe25f82e1
vidit-od <vidit894@gmail.com>
2023-04-14 10:57:38 +0530

    django setup for calculator

--- Imagine_Powertree/settings.py
+++ Imagine_Powertree/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "phonenumber_field",
-    'app'
+    'app',
+    'calculator'
 ]
 
 MIDDLEWARE = [
--- calculator/views.py
+++ calculator/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from django.http import HttpResponse,HttpResponseRedirect
+from django.urls import reverse
+import os
 
-# Create your views here.
+def calculator(request):
+    return render(request, 'calculator.html')
