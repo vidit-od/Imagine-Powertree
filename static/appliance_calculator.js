9e930b774565ff3906db163135c59be6af38bc6a
vidit-od <vidit894@gmail.com>
2023-04-20 15:02:13 +0530

    appliances

--- static/appliance_calculator.js
+++ static/appliance_calculator.js
@@ -7,6 +7,8 @@ let all_time = document.querySelectorAll("#time");
 let all_total_units = document.querySelectorAll('#total_units');
 let all_delete = document.querySelectorAll('#delete')
 console.log(all_delete);
+let submit = document.getElementById('submit');
+let max_rows = 13;
 
 all_quantity.forEach(function(node,index){
     node.addEventListener('input',function(){
@@ -40,10 +42,12 @@ all_delete.forEach(function(node,index){
     node.addEventListener('click',function(){
         let srno = node.parentNode.children[0].innerHTML
         delete_helper(srno);
+        max_rows--;
     })
 })
 
 function delete_helper(srno){
+    console.log('hi');
     let all_rows = document.querySelectorAll('.row');
     for( i=0; i<all_rows.length; i++){
         let curr = all_rows[i].children[0].innerHTML;
@@ -63,3 +67,43 @@ function srno_change(srno){
         pos++;
     }
 }
+
+function update(){
+    table = document.querySelector('#table');
+    all_srno = document.querySelectorAll('#srno');
+    all_watts = document.querySelectorAll("#watts");
+    all_quantity = document.querySelectorAll("#quantity");
+    all_total_watts = document.querySelectorAll("#total_watts")
+    all_time = document.querySelectorAll("#time");
+    all_total_units = document.querySelectorAll('#total_units');
+    all_delete = document.querySelectorAll('#delete');
+    submit = document.getElementById('submit');
+
+}
+submit.addEventListener('click',function(){
+    let input = document.getElementById('new_appliance').value;
+    document.getElementById('new_appliance').value = "";
+    
+    var row = table.insertRow(max_rows+1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var cell4 = row.insertCell(3);
+    var cell5 = row.insertCell(4);
+    var cell6 = row.insertCell(5);
+    var cell7 = row.insertCell(6);
+    var cell8 = row.insertCell(7);
+
+    cell1.innerHTML = max_rows + 1;
+    cell2.innerHTML = input;
+    cell3.innerHTML = '<input type="number" min="0" name="" id="watts"  value="60">';
+    cell4.innerHTML = '<input type="number" min="0" name="" id="quantity" value="0">';
+    cell5.innerHTML = '';
+    cell6.innerHTML = '<input type="number" min="0" name="" id="time" value="0">'
+    cell7.innerHTML = '';
+    cell8.innerHTML = '<button>x</button>';
+    max_rows++;
+
+    update();
+    console.log(table);
+})
